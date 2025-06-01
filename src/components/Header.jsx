355e83af2079ca6201cefe9685fd4fc164ead622
ahmadadptr001 <ahmadadptr@gmail.com>
2025-06-01 14:47:15 +0700

    memperbaiki tampilan ui items gambar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,18 +4,6 @@ export default function Header() {
         return (
                 <header className="sticky top-0 flex items-center justify-between p-7 bg-sky-100">
                         <a href="/" className="navbar-brand font-extrabold text-2xl">Ahmad QR</a>
-                        <nav className=" items-center gap-4 hidden sm:flex">
-                                <li className="nav-item">
-                                        <a href="" className="hover:!underline"><i class="fa-solid fa-phone-volume"></i> Hubungi Admin</a>
-                                </li>
-                                <li className="nav-item">
-                                        <a href="" className="hover:!underline"><i class="fa-solid fa-circle-question"></i> Tentang Admin</a>
-                                </li>
-                        </nav>
-
-                        <button type="button" className="md:hidden px-3 py-2 rounded-md bg-transparent">
-                                <i class="fa-solid fa-bars text-xl font-extrabold"></i>
-                        </button>
                 </header>
         )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,6 +8,7 @@ export default function Hero() {
         const arr = Array.from({ length: 25 }, (_, i) => i);
         
         return (
+                <>
                 <div className="grid grid-cols-1 sm:grid-cols-2 p-4 gap-4">
                         <div className="flex justify-end">
                                 <img src={ imageHero } alt="stiker welcome" className="w-[300px] sm:w-[500px] object-cover"/>
@@ -16,17 +17,18 @@ export default function Hero() {
                                 <p className="font-bold text-4xl text-sky-600">Welcome to my qr code Image</p>
                                 <p className="font-semibold text-xl text-sky-600">by Ahmad Adptr Develop</p>
                         </div>
-
-                        <div className="grid gap-4 grid-cols-2 mt-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 text-sm">
-                                        { arr.map((item, index) => (
-                                                <div onClick={() => navigate(`/image${index+1}`)} className="rounded-md bg-sky-500 hover:bg-sky-600 px-4 py-3 text-white cursor-pointer">
-                                                        <p className="flex gap-2 items-center">
-                                                                <i className="fas fa-image"></i>
-                                                                Item - { index + 1 }
-                                                        </p>
-                                                </div>
-                                        )) }
-                        </div>
                 </div>
+
+                <div className="grid gap-4 grid-cols-2 mt-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 p-5">
+                                { arr.map((item, index) => (
+                                        <div onClick={() => navigate(`/image${index+1}`)} className="rounded-md bg-sky-500 hover:bg-sky-600 px-4 py-3 text-white cursor-pointer">
+                                                <p className="flex gap-2 items-center">
+                                                        <i className="fas fa-image"></i>
+                                                        Item - { index + 1 }
+                                                </p>
+                                        </div>
+                                )) }
+                </div>
+                </>
         )
 }
